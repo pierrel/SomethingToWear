f02f27fde4053e32eb71b82879780c3f8efab125
Pierre Larochelle <pierre.larochelle@gatech.edu>
2009-10-15 19:02:22 -0400

    moved the main html into a new partial to be loaded dynamically

--- couch/_attachments/application/app.js
+++ couch/_attachments/application/app.js
@@ -111,58 +111,68 @@ var app = $.sammy(function() { with(this) {
         Mannequin.pant_id = '4ab8eb6901ee17def7dc670dcc4ffdaf';
         Mannequin.shoes_id = '18170179a9ce4ebcab91979881386f4c';
         
-        Mannequin.draw();
-        
-        fill_closet(context);
-        
-        
-        // set up mannequin-canvas interaction
-        Mannequin.element().droppable({
-            drop: function(evt, ui) {
-                draggable_info = ui.draggable.attr('id').split("-");
-                type = draggable_info[0];
-                id = draggable_info[1];
-                
-                if (type == 'shirts') {
-                    Mannequin.shirt_id = id;
-                } else if (type == 'pants') {
-                    Mannequin.pant_id = id;
-                } else if (type == 'shoes') {
-                    Mannequin.shoes_id = id;
-                }
-                
+        partial('templates/main.html', {}, function(rendered) {
+            $('#body').html(rendered);
+            
+            
+            
+            $('#main').ready(function() {
                 Mannequin.draw();
-            },
-            accept: '.piece',
-            activeClass: 'mannequin-canvas-dragging'
-        });
-        
-        
-        $('.search').click(function(evt) {
-            this.select();
-        });
-        
-        select_function = function(elt) { return elt != "" }; // use this to select only non-empty strings as attributes
-        $('#shirt-search').keyup(function(evt) {
-            var value = $(this).attr('value');
-            var attributes = arr_select(comma_separate(value), select_function);
+                fill_closet(context);
+
+
+                // set up mannequin-canvas interaction
+                Mannequin.element().droppable({
+                    drop: function(evt, ui) {
+                        draggable_info = ui.draggable.attr('id').split("-");
+                        type = draggable_info[0];
+                        id = draggable_info[1];
+
+                        if (type == 'shirts') {
+                            Mannequin.shirt_id = id;
+                        } else if (type == 'pants') {
+                            Mannequin.pant_id = id;
+                        } else if (type == 'shoes') {
+                            Mannequin.shoes_id = id;
+                        }
+
+                        Mannequin.draw();
+                    },
+                    accept: '.piece',
+                    activeClass: 'mannequin-canvas-dragging'
+                });
+
+
+                $('.search').click(function(evt) {
+                    this.select();
+                });
+
+                select_function = function(elt) { return elt != "" }; // use this to select only non-empty strings as attributes
+                $('#shirt-search').keyup(function(evt) {
+                    var value = $(this).attr('value');
+                    var attributes = arr_select(comma_separate(value), select_function);
+
+                    search_and_update_closet(context, 'shirt', 'shirts', attributes);
+                });
+
+                $('#pants-search').keyup(function(evt) {
+                    var value = $(this).attr('value');
+                    var attributes = arr_select(comma_separate(value), select_function);
+
+                    search_and_update_closet(context, 'pants', 'pants', attributes);
+                });
+
+                $('#shoes-search').keyup(function(evt) {
+                    var value = $(this).attr('value');
+                    var attributes = arr_select(comma_separate(value), select_function);
+
+                    search_and_update_closet(context, 'shoes', 'shoes', attributes);
+                });
+            });
             
-            search_and_update_closet(context, 'shirt', 'shirts', attributes);
-        });
-        
-        $('#pants-search').keyup(function(evt) {
-            var value = $(this).attr('value');
-            var attributes = arr_select(comma_separate(value), select_function);
             
-            search_and_update_closet(context, 'pants', 'pants', attributes);
-        });
+        });        
         
-        $('#shoes-search').keyup(function(evt) {
-            var value = $(this).attr('value');
-            var attributes = arr_select(comma_separate(value), select_function);
-            
-            search_and_update_closet(context, 'shoes', 'shoes', attributes);
-        });
     }});
     
     get('#/rate', function() { with(this) {
