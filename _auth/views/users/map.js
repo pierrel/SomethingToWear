216b8c02971c4aed3c9283d7f51588239c4044d2
Pierre Larochelle <pierre.larochelle@gatech.edu>
2009-11-03 22:19:53 -0500

    changed role of user in db from admin to user

--- _auth/views/users/map.js
+++ _auth/views/users/map.js
@@ -1,5 +1,5 @@
 function(doc) {
     if (doc.doc_type == 'user') {
-        emit(doc._id, {password_sha: doc.password, salt: "", secret: 'halloween', roles: ['_admin']});
+        emit(doc._id, {password_sha: doc.password, salt: "", secret: 'halloween', roles: ['user']});
     }
 }
