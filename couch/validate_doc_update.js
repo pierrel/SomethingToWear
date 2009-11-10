5a4b5dcaca7978d7750be7cd88d889c2d5129fb9
Pierre Larochelle <pierre.larochelle@gatech.edu>
2009-11-10 16:02:08 -0500

    added validation

--- couch/validate_doc_update.js
+++ couch/validate_doc_update.js
@@ -3,6 +3,6 @@ function (newDoc, oldDoc, user) {
     isAdmin = (user.roles.indexOf('_admin') != -1)
     
     if (!isAdmin) {
-        // TODO: Make validations    
-    }    
+        throw {unauthorized: "Can't updat documents unless you're an admin"}
+    } 
 }
