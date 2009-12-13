844969e09a79c2e16e57368af889b56bddb3d73d
Pierre Larochelle <pierre.larochelle@gatech.edu>
2009-12-13 18:26:37 -0500

    removed loading images from the closet

--- sammy/application/lib/closet.js
+++ sammy/application/lib/closet.js
@@ -81,9 +81,6 @@ function piece_to_closet_part(context, ids, part_name) {
         image.onload = function(part_name, id, ids) {
             return function() {
                 var piece_image = $('#' + part_name + '-' + id);
-                piece_image.removeClass('loading-piece');
-                piece_image.addClass('piece-image');
-                piece_image.attr('src', piece_image_url(id));
                 // if the piece is in the mannequin then highlight it
                 if (Mannequin[transform[part_name]] == id) {
                     highlight_piece(part_name, id);
@@ -91,7 +88,7 @@ function piece_to_closet_part(context, ids, part_name) {
                 
                 // change the width of the part
                 var part = $('#' + part_name);
-                part.width((part.width() + parseInt(piece_image.css('width')) + 10) + 'px');
+                part.width((part.width() + parseInt(piece_image.css('width')) + 7) + 'px');
                 
                 // call fill_closet_part on the rest of the ids
                 piece_to_closet_part(context, ids, part_name);
