91f8f276f63ed30abe11784aaa03c208d874173c
Pierre Larochelle <pierre.larochelle@gatech.edu>
2010-01-14 18:07:07 -0500

    updated the ready_to_show function to exclude unavailable pieces

--- couch/lib/piece.js
+++ couch/lib/piece.js
@@ -6,6 +6,6 @@ function ready_to_show(doc) {
         has_descriptors = has_descriptors && doc[part_name] && doc[part_name].length >= 2;
     }
     
-    return doc.placement && doc._attachments && has_descriptors && doc.name && doc.uncut_image_url && !doc.cut_wrong ;
+    return doc.placement && doc._attachments && has_descriptors && doc.name && doc.uncut_image_url && !doc.cut_wrong && (doc.available == null || doc.available);
     
 };
